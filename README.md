8f77039e02b0ac78e80aba88c08976731be115e6
gulser <glserozkan@gmail.com>
2024-12-05 15:43:41 +0100

    docker-compose file is added and working

--- README.md
+++
@@ -1,5 +0,0 @@
-To start container -for now- :
-
-cd docker
-
-./build_run.sh
--- docker/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# apply database migrations
-echo "Running migrations..."
-python manage.py migrate
-
-echo "Starting Django server..."
-exec "$@"
