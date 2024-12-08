dd71ca2dd190a1e9bf4b1cf1328725866b8d9697
gulser <glserozkan@gmail.com>
2024-12-08 17:45:09 +0100

    test_list_players is added

--- matchmaking_/matchmaking_service/tests.py
+++ matchmaking_/matchmaking_service/tests.py
@@ -10,7 +10,7 @@ class MatchViewTest(TestCase):
         
         PlayerQueue.objects.create(player_id="player1", total_wins=10)
         PlayerQueue.objects.create(player_id="player2", total_wins=8)
-    
+
     def test_join_queue(self):
         response = self.client.post(reverse('join_queue'), {
             'player_id': 'player3',
@@ -28,3 +28,18 @@ class MatchViewTest(TestCase):
         match = Match.objects.first()
         self.assertEqual(match.player1, "player1")
         self.assertEqual(match.player2, "player2")
+
+    def test_list_players(self):
+        response = self.client.get(reverse('list_players'), format='json')
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(PlayerQueue.objects.count(), 2)
+        self.assertEqual(PlayerQueue.objects.filter(player_id="player1").exists(), True)
+        self.assertEqual(PlayerQueue.objects.filter(player_id="player2").exists(), True)
+
+        self.client.post(reverse('join_queue'), {
+            'player_id': 'player4',
+            'total_wins': 3
+        }, format='json')
+
+        self.assertEqual(PlayerQueue.objects.filter(player_id="player4").exists(), True)
