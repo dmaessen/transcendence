2f8e1656e60bd2e6de344285edc2a11f4274e21f
Laura <eccentricporcupine@gmail.com>
2024-12-18 17:00:58 +0100

    still don't get it

--- backend/database/init_database.sh
+++ backend/database/init_database.sh
@@ -1,30 +1,16 @@
 #!/bin/bash
 
-# Environment variables passed by Docker Compose
-DB_ENGINE=${DB_ENGINE}
-DB_HOST=${POSTGRES_HOST}
-DB_PORT=${POSTGRES_PORT}
-DB_USER=${POSTGRES_USER}
-DB_PASSWORD=${POSTGRES_PASSWORD}
-DB_NAME="psql"
-
-# Wait for PostgreSQL to be available
-until psql -h "$DB_HOST" -U "$DB_USER" -c '\q'; do
-  echo "Postgres is unavailable - retrying in 5 seconds"
-  sleep 5
-done
-
 # Check if the database exists
-echo "Checking if database $DB_NAME exists..."
-DB_EXISTS=$(PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -tc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME';")
+# echo "Checking if database $POSTGRES_NAME exists..."
+# DB_EXISTS=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -tc "SELECT 1 FROM pg_database WHERE datname = '$POSTGRES_NAME';")
 
-if [[ $DB_EXISTS == 1 ]]; then
-    echo "Database $DB_NAME already exists."
-else
-    echo "Database $DB_NAME does not exist. Creating it..."
-    PGPASSWORD=$DB_PASSWORD createdb -h $DB_HOST -U $DB_USER $DB_NAME
-    echo "Database $DB_NAME created successfully."
-fi
+# if [[ $DB_EXISTS == 1 ]]; then
+#     echo "Database $POSTGRES_NAME already exists."
+# else
+#     echo "Database $POSTGRES_NAME does not exist. Creating it..."
+#     PGPASSWORD=$POSTGRES_PASSWORD createdb -h $POSTGRES_HOST -U $POSTGRES_USER $POSTGRES_NAME
+#     echo "Database $POSTGRES_NAME created successfully."
+# fi
 
 # Run Django migrations
 echo "Running Django migrations..."
