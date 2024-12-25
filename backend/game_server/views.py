ce68c5d4c7b912b8113df2d030f2383b8b6d0ab8
gulser <glserozkan@gmail.com>
2024-12-25 20:31:19 +0100

    localhost:8000/game_server displays staticsssss

--- backend/game_server/views.py
+++ backend/game_server/views.py
@@ -7,6 +7,6 @@ def index(request):
     return render(request, 'game_server/index.html')
 
 # def start_game(request):
-    if request.method == "POST":
-        game = Game(mode="singleplayer")  # ex
-        return JsonResponse({"status": "Game started", "game_id": id(game)})
+    # if request.method == "POST":
+    #     game = Game(mode="singleplayer")  # ex
+    #     return JsonResponse({"status": "Game started", "game_id": id(game)})
