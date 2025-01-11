41402e8f3f7516ad9d0d3fed9944f645537bdfa3
gulser <glserozkan@gmail.com>
2025-01-11 20:09:49 +0100

    examine consumers

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -13,15 +13,19 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.urls import path
-from game_server.websocket_client import GameConsumer
+from game_server.consumers import GameConsumer
 from backend.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
 #application = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         # URLRouter({
         #     "game_server/": GameConsumer.as_asgi(),
@@ -30,3 +34,53 @@ application = ProtocolTypeRouter({
         URLRouter(websocket_urlpatterns)
     ),
 })
+
+# from django.core.asgi import get_asgi_application
+# from django.urls import re_path
+
+# # Initialize Django ASGI application early to ensure the AppRegistry
+# # is populated before importing code that may import ORM models.
+# django_asgi_app = get_asgi_application()
+
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from channels.security.websocket import AllowedHostsOriginValidator
+
+# application = ProtocolTypeRouter({
+#     "http": django_asgi_app,
+#     "websocket": AllowedHostsOriginValidator(
+#         AuthMiddlewareStack(
+#             URLRouter([
+#                 re_path(r"^front(end)/$", consumers.AsyncChatConsumer.as_asgi()),
+#             ])
+#         )
+#     ),
+# })
+
+#Channels introduces the idea of a channel layer, a low-level abstraction around a set of transports
+# that allow you to send information between different processes.
+# Each application instance has a unique channel name, and can join groups, allowing both point-to-point and broadcast messaging.
+
+
+
+# from django.core.asgi import get_asgi_application
+# from django.urls import re_path
+
+# # Initialize Django ASGI application early to ensure the AppRegistry
+# # is populated before importing code that may import ORM models.
+# django_asgi_app = get_asgi_application()
+
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.auth import AuthMiddlewareStack
+# from channels.security.websocket import AllowedHostsOriginValidator
+
+# application = ProtocolTypeRouter({
+#     "http": django_asgi_app,
+#     "websocket": AllowedHostsOriginValidator(
+#         AuthMiddlewareStack(
+#             URLRouter([
+#                 re_path(r"^front(end)/$", consumers.AsyncChatConsumer.as_asgi()),
+#             ])
+#         )
+#     ),
+# })
--- backend/backend/routing.py
+++ backend/backend/routing.py
@@ -1,7 +1,7 @@
 # routing.py
 from django.urls import re_path # remove?
 from django.urls import path
-from game_server.websocket_client import GameConsumer
+from game_server.consumers import GameConsumer
 
 websocket_urlpatterns = [
     #re_path(r'ws/game_server/$', GameConsumer.as_asgi()),
