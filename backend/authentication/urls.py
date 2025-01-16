3bcfa95c22ec623fb7e01425adce76040807ade1
Alexander Von benckendorff <avon-ben@f0r2s12.codam.nl>
2025-01-16 19:02:12 +0100

    registration_broken

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.sign_in, name='sign_in'),
+	path('', views.home, name='home'),
+	path('sign_out', views.sign_out, name= 'sign_out'),
+	path('sign_in', views.sign_in, name='sign_in'),
+	path('register', views.register, name='register')
 ]
--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# dont want to make unecessary migrations everytime i make an image
-# run migrations only if there are unapplied changes
-echo "Checking for pending migrations..."
-python manage.py makemigrations --check --dry-run
-
-if [ $? -eq 0 ]; then
-  echo "Applying migrations..."
-  python manage.py migrate --run-syncdb
-else
-  echo "No migrations needed."
-fi
+python manage.py makemigrations
+python manage.py migrate
 
 echo "Starting Django server..."
 exec python manage.py runserver 0.0.0.0:8000
