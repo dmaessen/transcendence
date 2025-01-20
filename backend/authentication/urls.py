d6836b8b51fdf9ce0c92503283ff74a19fdab4cd
Alexander Von benckendorff <avon-ben@f0r2s13.codam.nl>
2025-01-20 16:16:54 +0100

    database woooooorksgit add *

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
 	path('', views.home, name='home'),
 	path('sign_out', views.sign_out, name= 'sign_out'),
 	path('sign_in', views.sign_in, name='sign_in'),
-	path('register', views.register, name='register')
+	path('register', views.register, name='register'),
     path('42/', include('authentication.providers.forty_two.urls')),  # Include FortyTwo OAuth routes]
 ]
