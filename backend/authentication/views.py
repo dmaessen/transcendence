d06a27c13bdb5e3252116bba4eabd65bfb978c82
alexbenkey <alex@vonbenckendorff.net>
2025-01-27 11:33:14 +0100

    pre_pull

--- backend/authentication/views.py
+++ backend/authentication/views.py
@@ -8,22 +8,25 @@ from django.http import HttpResponse, JsonResponse
 
 def sign_in(request):
 	if request.method == 'GET':
+		# user = authenticate(email = 'email', password = 'password')
 		if request.user.is_authenticated:
-			return render(request, 'home/html')
+			messages.succes(request, f'already signed in')
+			return redirect('game_server')
 		form = LoginForm()
 		return render(request, 'users/login.html', {'form':form})
 	
-	elif request.method == 'POST':
+	if request.method == 'POST':
 		form = LoginForm(request.POST)
 
 		if form.is_valid():
-			name = form.cleaned_data['name']
+			email = form.cleaned_data['email']
 			password = form.cleaned_data['password']
-			user = authenticate(request,username=name,password=password)
+			user = authenticate(request,email=email,password=password)
 			if user:
-				login(request, user)
-				messages.success(request,f'Hi {name.title()}, welcome back!')
-				return redirect('game_server') #this should probably be home, also SPA?
+				# login(request, user)
+				# messages.success(request,f'Hi {name.title()}, welcome back!')
+				messages.success(request,f'Hi user welcome back!')
+				return redirect('users/valid.html') #this should probably be home, also SPA?
 	
 	messages.error(request,f'Invalid username or password')
 	return render(request, 'users/login.html',{'form': form})
@@ -49,10 +52,33 @@ def register(request):
 			user.save()
 			messages.success(request, 'you have signed up sucesfully.')
 			login(request, user)
-			return redirect('sign_in')
+			return redirect('sign_out')
 		else:
 			form = RegisterForm()
 			return render(request, 'users/register.html', {'form': form})
 
 def home(request):
 	return render(request, 'base.html')
+
+def get_email(request):
+    try:
+        user = request.user  
+        if user.is_authenticated:
+            email = user.email
+            return JsonResponse({'email': email})
+        else:
+            return JsonResponse({'error': 'User is not authenticated'}, status=401)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
+
+
+def get_nickname(request):
+    try:
+        user = request.user
+        if user.is_authenticated:
+            nickname = user.nickname  
+            return JsonResponse({'nickname': nickname})
+        else:
+            return JsonResponse({'error': 'User is not authenticated'}, status=401)
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=400)
