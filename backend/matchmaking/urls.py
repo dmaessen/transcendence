932edcb44b687f179a9eaa326cc7e3f7cf5cc4c4
gulser <glserozkan@gmail.com>
2025-01-30 00:01:21 +0100

    create_a_match is still broken, force_auth is working in tests

--- backend/matchmaking/urls.py
+++ backend/matchmaking/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('api/players/', views.list_players, name='list_players'),
+    #path('api/players/', views.list_players, name='list_players'),
     path('api/create_a_match/', views.create_a_match, name='create_a_match'),
     path('api/create_matches/', views.create_matches, name='create_matches'),
     path('api/matches/', views.list_matches, name='matches'),
