7a1a35f1475adbabcf7893b705f30034588bf11d
Laura <eccentricporcupine@gmail.com>
2025-03-13 15:19:47 +0100

    edit user not the prety but working

--- backend/data/urls.py
+++ backend/data/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("api/userData/", get_user_data, name="get_user_data"),
     path("api/userMatches/", get_user_matches, name="get_user_matches"),
     path("api/userTournaments/", get_user_tournaments, name="get_user_tournaments"),
+    path("api/editProfile/", edit_user_data, name="edit_user_data"),
 ]
 
 
