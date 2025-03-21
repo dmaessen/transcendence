59cd58fc19a518d46f86e4e844b6e6bfba386ad6
gulser <glserozkan@gmail.com>
2025-03-21 12:39:12 +0100

    before merge

--- monitoring/kibana/ilm_setup.sh
+++ monitoring/kibana/ilm_setup.sh
@@ -38,7 +38,6 @@ curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT "https://elasticsear
   }
 }'
 
-#curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT "https://elasticsearch:9200/_data_stream/test-logs-default"
 
 # Şu anki tarihi ISO formatında al
 # CURRENT_DATE=$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ")
@@ -47,12 +46,71 @@ curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT "https://elasticsear
 # curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X POST "https://elasticsearch:9200/logs-default/_doc" -H 'Content-Type: application/json' -d'
 # {
 #   "@timestamp": "'$CURRENT_DATE'",
-#   "message": "Test log 1"
-# }'
-
-# curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X POST "https://elasticsearch:9200/logs-default/_doc?refresh=true" -H 'Content-Type: application/json' -d'
-# {
-#   "@timestamp": "'"$(date -u +%Y-%m-%dT%H:%M:%S.%3NZ)"'",
 #   "message": "Data stream initialized"
 # }'
+
+
+curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X POST "https://elasticsearch:9200/logs-generic-default/_rollover"
+
+
 exec /usr/local/bin/kibana-docker
+
+
+# #!/bin/bash
+
+# sleep 30
+
+# # ILM politikası oluşturma
+# curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT "https://elasticsearch:9200/_ilm/policy/logs-policy" -H 'Content-Type: application/json' -d'
+# {
+#   "policy": {
+#     "phases": {
+#       "hot": {
+#         "min_age": "0ms",
+#         "actions": {
+#           "rollover": {
+#             "max_age": "2m",
+#             "max_docs": 100
+#           }
+#         }
+#       },
+#       "delete": {
+#         "min_age": "4m",
+#         "actions": {
+#           "delete": {}
+#         }
+#       }
+#     }
+#   }
+# }'
+
+# # Index template oluşturma
+# curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT "https://elasticsearch:9200/_index_template/logs-template" -H 'Content-Type: application/json' -d'
+# {
+#   "index_patterns": ["logs-*-*"],
+#   "data_stream": { },
+#   "priority": 500,
+#   "template": {
+#     "settings": {
+#       "index.lifecycle.name": "logs-policy"
+#     }
+#   }
+# }'
+
+# sleep 10
+
+# # İlk oluşturulan data stream index'ini `logs-policy` ILM politikasına bağla
+# FIRST_INDEX=$(curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -s "https://elasticsearch:9200/_data_stream/logs-generic-default" | jq -r '.data_streams[0].indices[0].index_name')
+
+# curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X PUT "https://elasticsearch:9200/${FIRST_INDEX}/_settings" -H 'Content-Type: application/json' -d'
+# {
+#   "index.lifecycle.name": "logs-policy"
+# }'
+
+# # Rollover işlemini tetikle
+# curl -k -u "${ELASTIC_USERNAME}:${ELASTIC_PASSWORD}" -X POST "https://elasticsearch:9200/logs-generic-default/_rollover"
+
+# exec /usr/local/bin/kibana-docker
+
+
+
