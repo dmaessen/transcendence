6ee0bf6301e30acf9637ec0678a8657424f0d776
Laura <eccentricporcupine@gmail.com>
2025-05-04 16:45:11 +0200

    fixed frontend and close button

---
+++ static/frontend/js/add_friends.js
@@ -0,0 +1,196 @@
+async function acceptFriend(friendshipID) {
+    console.log("friendshipID: ", friendshipID);
+    try {
+        const response = await fetch(`/data/api/acceptFriendship/`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("access_token")}`, // if needed for authentication
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({friendshipID: friendshipID})
+        });
+        if (!response.ok) {
+            throw new Error(`Error, status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data.success) {
+            loadFriendsRequests();
+            console.log("Friend added successfully");
+        } else {
+            console.error("Failed to add friend:", data.error);
+        }
+    } catch (error) {
+        console.error("Error adding a friend:", error);
+    }
+}
+
+async function declineFriend(friendshipID) {
+    console.log("friendshipID: ", friendshipID);
+    try {
+        const response = await fetch(`/data/api/cancelFriendship/`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("access_token")}`, // if needed for authentication
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({friendshipID: friendshipID})
+        });
+        if (!response.ok) {
+            throw new Error(`Error, status: ${response.status}`);
+        }
+        const data = await response.json();
+        if (data.success) {
+            loadFriendsRequests();
+            console.log("Friend added successfully");
+        } else {
+            console.error("Failed to add friend:", data.error);
+        }
+    } catch (error) {
+        console.error("Error adding a friend:", error);
+    }
+}
+
+function populateFRequests(data) {
+    console.log("populateFriendsRequest: ", data);
+    if (!Array.isArray(data)) {
+        console.error("Provided data is not an array:", data);
+        return;
+    }
+
+    // Clear existing rows
+    frienshipRequestTable.innerHTML = "";
+
+    // Create a table head and append headers
+    let thead = frienshipRequestTable.querySelector('thead');
+    if (!thead) {
+        thead = document.createElement('thead');
+        frienshipRequestTable.appendChild(thead);
+    }
+    let headerRow = document.createElement("tr");
+    headerRow.innerHTML = "<th>Username</th><th>Accept</th><th>Decline</th>";
+    thead.appendChild(headerRow);
+
+    data.forEach(item => {
+        const row = document.createElement("tr");
+        
+        const usernameCell = document.createElement("td");
+        if(item.user_id === item.sender_id){
+            usernameCell.textContent = item.receiver;
+        } else {
+            usernameCell.textContent = item.sender;
+        }
+        row.appendChild(usernameCell);
+        
+        const acceptCell = document.createElement("td");
+        if(item.user_id != item.sender_id) {
+            const acceptLink = document.createElement("span");
+            acceptLink.textContent = "✔️";
+            acceptLink.dataset.friendshipID = item.friendship_id;
+            acceptLink.style.cursor = "pointer"; // Make it look clickable
+            acceptLink.style.color = "green"; // Make it look like a link
+            acceptLink.addEventListener("click", function(){
+            acceptFriend(acceptLink.dataset.friendshipID);
+            });
+            acceptCell.appendChild(acceptLink);
+        } else {
+            acceptCell.textContent = "";
+        }
+        row.appendChild(acceptCell);
+
+        const declineCell = document.createElement("td");
+        const declineLink = document.createElement("span");
+        declineLink.textContent = "❌";
+        declineLink.dataset.friendshipID = item.friendship_id;
+        declineLink.style.cursor = "pointer"; // Make it look clickable
+        declineLink.addEventListener("click", function(){
+           declineFriend(declineLink.dataset.friendshipID);
+        });
+        declineCell.appendChild(declineLink);
+        row.appendChild(declineCell);
+
+        frienshipRequestTable.appendChild(row);
+    });
+}
+
+async function loadFriendsRequests() {
+    console.log("loadFRequest");
+    try {
+        const response = await fetch(`/data/api/friendsRequests`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("access_token")}`, // if needed for authentication
+            }
+        });
+        if (!response.ok) {
+            throw new Error(`error, status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        console.log("Raw response:", data);
+        
+        if (Array.isArray(data)) {
+            populateFRequests(data);
+        } else {
+            console.error("Data.fRequests is not an array:", data);
+        }
+        
+    } catch(error) {
+        console.error("Error fetching matches data:", error);
+    }
+}
+
+const addFriendBtn = document.getElementById("addFriendsBtn");
+if(addFriendBtn) {
+    addFriendBtn.addEventListener("click", loadFriendsRequests);
+}
+
+const searchFriendBtn = document.getElementById("searchFriendBtn");
+if (searchFriendBtn) {
+    searchFriendBtn.addEventListener("click", async function (event) {
+        try{
+            event.preventDefault();
+
+            const newFriendUsername = document.getElementById("newFriendUsername").value.trim();
+            if (!username) {
+                alert("Please enter a username.");
+                return;
+            }
+            
+            const response = await fetch(`/data/api/searchUser/?friendUsername=${newFriendUsername}`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("access_token")}`, // if needed for authentication
+                },
+            });
+            if (!response.ok) {
+                throw new Error(`Error, status: ${response.status}`);
+            }
+    
+            const data = await response.json();
+            const userID = data.user_id;
+            if (userID) {
+                const addFriendsModalElement = document.getElementById("addFriendsModal");
+                const addFriendsModal = bootstrap.Modal.getInstance(addFriendsModalElement);
+                addFriendsModal.hide();
+                loadProfile(userID);
+            } else {
+                alert("User not found");
+            }
+        } catch (error) {
+            console.log("Error on finding friend: ", error);
+        }
+    });
+}
+
+const addFriendCloseBtn = document.getElementById("addFriendCloseBtn");
+if(addFriendCloseBtn){
+    addFriendCloseBtn.addEventListener("click", function() {
+        console.log("add close clicked");
+        const addFriendsModalElement = document.getElementById("addFriendsModal");
+        const addFriendsModal = bootstrap.Modal.getInstance(addFriendsModalElement);
+        addFriendsModal.hide();
+        const friendsModalElement = document.getElementById("friendsModal");
+        const friendsModal = bootstrap.Modal.getOrCreateInstance(friendsModalElement);
+        friendsModal.show();
+    });
+}
---
+++ static/frontend/js/matches.js
@@ -0,0 +1,115 @@
+function populateAllMatches(data) {
+    // console.log("populateMatches data: ", data);
+    if (!Array.isArray(data)) {
+        console.error("Provided data is not an array:", data);
+        return;
+    }
+
+    // Clear existing rows
+    allMatchesTable.innerHTML = "";
+
+    // Create a table head and append headers
+    let thead = allMatchesTable.querySelector('thead');
+    if (!thead) {
+        thead = document.createElement('thead');
+        allMatchesTable.appendChild(thead);
+    }
+    let headerRow = document.createElement("tr");
+    headerRow.innerHTML = "<th>Match date</th><th>Winner</th><th>Opponent</th>";
+    thead.appendChild(headerRow);
+
+    //Populate
+    data.forEach(item => {
+        const row = document.createElement("tr");
+
+        const dateCell = document.createElement("td");
+        dateCell.textContent = item.match_start || "N/A";
+        row.appendChild(dateCell);
+        
+        const winnerCell = document.createElement("td");
+        if (item.opponent === item.winner_name) {
+            const winnerLink = document.createElement("span");
+            winnerLink.textContent = item.winner_name;
+            winnerLink.dataset.userID = item.opponentID;
+            winnerLink.style.cursor = "pointer"; // Make it look clickable
+            winnerLink.style.color = "gray"; // Make it look like a link
+
+            winnerLink.addEventListener("click", function(){
+                const allMatchesModalElement = document.getElementById("allMatchesModal");
+                const allMatchesModal = bootstrap.Modal.getInstance(allMatchesModalElement);
+                allMatchesModal.hide();
+                loadProfile(winnerLink.dataset.userID);
+            });
+            winnerCell.appendChild(winnerLink);
+        } else {
+            winnerCell.textContent = item.winner_name;
+        }
+        row.appendChild(winnerCell);
+
+        const opponentCell = document.createElement("td");
+        const opponentLink = document.createElement("span");
+        opponentLink.textContent = item.opponent;
+        opponentLink.dataset.userID = item.opponentID;
+        opponentLink.style.cursor = "pointer"; // Make it look clickable
+        opponentLink.style.color = "gray"; // Make it look like a link
+        opponentLink.addEventListener("click", function(){
+            const allMatchesModalElement = document.getElementById("allMatchesModal");
+            const allMatchesModal = bootstrap.Modal.getInstance(allMatchesModalElement);
+            allMatchesModal.hide();
+            loadProfile(opponentLink.dataset.userID);
+        });
+        opponentCell.appendChild(opponentLink);
+        row.appendChild(opponentCell);
+        
+        allMatchesTable.appendChild(row);
+    });
+}
+
+async function loadAllMatches(userID) {
+    try {
+        const response = await fetch(`/data/api/allUserMatches/?userID=${userID}`, {
+            method: "GET",
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("access_token")}`,
+                "Content-Type": "application/json",
+            },
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error, status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log("Raw response:", data);
+        console.log("Matches:", data.matches);
+
+        if (Array.isArray(data.matches)) {
+            populateAllMatches(data.matches);
+        } else {
+            console.error("Data.matches is not an array:", data.matches);
+        }
+    } catch (error) {
+        console.error("Error fetching matches data:", error);
+    }
+}
+
+const allMatchesLink = document.querySelector('[data-bs-toggle="modal"][data-bs-target="#allMatchesModal"]');
+if (allMatchesLink) {
+    allMatchesLink.addEventListener("click", function() {
+        const userID = document.getElementById("AllMatchesModal").dataset.userId;
+        loadAllMatches(userID);
+    });
+}
+
+const closeAllMatchesBtn = document.getElementById("closeAllMatchesBtn");
+if(closeAllMatchesBtn){
+    closeAllMatchesBtn.addEventListener("click", function() {
+        console.log("profile close clicked");
+        const allMatchesModalElement = document.getElementById("allMatchesModal");
+        const allMatchesModal = bootstrap.Modal.getInstance(allMatchesModalElement);
+        allMatchesModal.hide();
+        const profileModalElement = document.getElementById("profileModal");
+        const profileModal = bootstrap.Modal.getInstance(profileModalElement);
+        profileModal.show();
+    });
+}
