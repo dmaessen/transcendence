27ab49cc39324a3ff851525c25bc56d712691266
Laura <eccentricporcupine@gmail.com>
2025-05-20 12:13:21 +0200

    updated .env_exemple and added Django SECRET_KEY setting to .env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,7 +45,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)co%lac8eq8ki4lv9!&-t0&#%=(_y#(&=13b#nbilgk58rf&uz'
+SECRET_KEY =  os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
